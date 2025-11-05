ab16944bfa7b26185133db92ed0534eace104cbf
신현우 <healim5028@naver.com>
2025-11-05 17:57:37 +0900

    feat: 라우터 기본 레이아웃 틀 정리

--- front/src/app/router/Router.tsx
+++ front/src/app/router/Router.tsx
@@ -1,37 +1,52 @@
-import { BrowserRouter, Routes, Route, Outlet } from 'react-router-dom';
-import Home from '../../pages/home/Home';
-import Login from '../../pages/auth/Login';
+import { BrowserRouter, Routes, Route, Outlet, Navigate } from 'react-router-dom'
+import Home from '../../pages/home/Home'
+import Login from '../../pages/auth/Login'
+import { useSessionStore } from '../../state/session.store'
+import Header from '../layout/Header'
+import Sidebar from '../layout/Sidebar'
 
-const Common = () => {
-    return (
+const Layout = () => (
+  <div className="flex flex-col min-h-screen">
+    <Header />
+    <div className="flex flex-1">
+      <Sidebar />
+      <main className="flex-1 overflow-y-auto">
         <Outlet />
-    )
+      </main>
+    </div>
+  </div>
+)
+
+const PublicOnly = () => {
+  const { isAuthenticated } = useSessionStore()
+  return isAuthenticated ? <Navigate to="/home" replace /> : <Outlet />
 }
 
-const Protected = () => {
 
-
-
-    return (
-        <Outlet />
-    )
+const ProtectedLayout = () => {
+  const { isAuthenticated } = useSessionStore()
+  return isAuthenticated ? <Layout /> : <Navigate to="/" replace />
 }
 
+/** ----- 라우터 ----- */
 const Router = () => {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route element={<Common />}>
-                    <Route index path='/' element={<Home />} />
-                    <Route index path='/join' element={<Home />} />
-                    <Route index path='/login' element={<Login />} />
-                </Route>
-                <Route element={<Protected />}>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<PublicOnly />}>
+          <Route index element={<Login />} />          
+          <Route path="/login" element={<Login />} />  
+          <Route path="/join" element={<Home />} />    
+        </Route>
+        
+        <Route element={<ProtectedLayout />}>
+          <Route path="/home" element={<Home />} />          
+        </Route>
 
-                </Route>
-            </Routes>
-        </BrowserRouter>
-    )
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
-export default Router;
+export default Router
